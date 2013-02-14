decea3023477721f166d8e30866a809e8115d4fb
Carlos Barreto <carlos.barreto@live.com.mx>
2013-02-14 08:47:28 -0600

    Reparado HomeController...

---
+++ AgioNet/Controllers/HomeController.vb
@@ -0,0 +1,18 @@
+﻿Namespace AgioNet
+    Public Class HomeController
+        Inherits System.Web.Mvc.Controller
+
+        ' Methods
+        Public Function About() As ActionResult
+            Return Me.View
+        End Function
+
+        <Authorize> _
+        Public Function Index() As ActionResult
+            Me.ViewData.Item("Message") = "Welcome to ASP.NET MVC!"
+            Return Me.View
+        End Function
+
+    End Class
+
+End Namespace
